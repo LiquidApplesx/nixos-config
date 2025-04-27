32bc33c2021c151bc4d533c17855fd2b81602c08
LiquidApplesx <liquidapples@proton.me>
2025-04-27 18:00:15 -0400

    .

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -4,6 +4,10 @@
     enable = true;
 
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+    plugins = {
+      lualine.enable = true;
+      lazy.enable = true;
+      nix.enable = true;
+    };
   };
 }
